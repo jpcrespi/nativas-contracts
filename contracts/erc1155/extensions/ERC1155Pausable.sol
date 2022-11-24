055553f4ad074e3754b582a92858cd03482379e4
Juan Pablo Crespi <juanpablocrespi99@gmail.com>
2022-11-24 18:35:18 -0300

    Update pausable extension

--- contracts/erc1155/extensions/ERC1155Pausable.sol
+++ contracts/erc1155/extensions/ERC1155Pausable.sol
@@ -4,13 +4,53 @@
 
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/security/Pausable.sol";
 import "../ERC1155.sol";
 
 /**
  * @dev ERC1155 pause implementation
  */
-contract ERC1155Pausable is Pausable, ERC1155 {
+contract ERC1155Pausable is ERC1155 {
+    /**
+     * @dev Emitted when the pause is triggered by `account`.
+     */
+    event Paused(address account);
+
+    /**
+     * @dev Emitted when the pause is lifted by `account`.
+     */
+    event Unpaused(address account);
+
+    bool private _paused;
+
+    /**
+     * @dev Initializes the contract in unpaused state.
+     */
+    constructor() {
+        _paused = false;
+    }
+
+    /**
+     * @dev Returns true if the contract is paused, and false otherwise.
+     */
+    function paused() public view virtual returns (bool) {
+        return _paused;
+    }
+
+    /**
+     * @dev Triggers stopped state.
+     */
+    function _pause() internal virtual {
+        _paused = true;
+        emit Paused(_msgSender());
+    }
+
+    /**
+     * @dev Returns to normal state.
+     */
+    function _unpause() internal virtual {
+        _paused = false;
+        emit Unpaused(_msgSender());
+    }
     /**
      * @dev See {ERC1155-_beforeTokenTransfer}.
      *

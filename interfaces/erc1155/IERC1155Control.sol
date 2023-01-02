0c75d4ca68c27a4751f632765cc5076365b8360e
Juan Pablo Crespi <juanpablocrespi99@gmail.com>
2023-01-02 15:03:19 -0300

    Add comments

--- interfaces/erc1155/IERC1155Control.sol
+++ interfaces/erc1155/IERC1155Control.sol
@@ -8,17 +8,38 @@ pragma solidity ^0.8.0;
  * @title
  */
 interface IERC1155Control {
+    /**
+     * @dev Returns `true` if `account` has been granted `admin` role.
+     */
     function isAdmin(address account) external returns (bool);
 
+    /**
+     * @dev Returns `true` if `account` has been granted `burner` role.
+     */
     function isBurner(address account) external returns (bool);
 
+    /**
+     * @dev Returns `true` if `account` has been granted `adapter` role.
+     */
     function isAdapter(address account) external returns (bool);
 
+    /**
+     * @dev Returns `true` if `account` has been granted `minter` role.
+     */
     function isMinter(address account) external returns (bool);
 
+    /**
+     * @dev Returns `true` if `account` has been granted `pauser` role.
+     */
     function isPauser(address account) external returns (bool);
 
+    /**
+     * @dev Returns `true` if `account` has been granted `editor` role.
+     */
     function isEditor(address account) external returns (bool);
 
+    /**
+     * @dev Returns `true` if `account` has been granted `swapper` role.
+     */
     function isSwapper(address account) external returns (bool);
 }

5f738ce174ca7faf3ad5d0a2a1bb5d3ccd0963f1
Juan Pablo Crespi <juanpablocrespi99@gmail.com>
2023-07-31 10:30:51 -0300

    FIX: vscode settings

--- tests/AccessControl.ts
+++ tests/AccessControl.ts
@@ -1,19 +1,19 @@
-const AccessControlMock = artifacts.require('AccessControlMock');
+const AccessControlMock = artifacts.require('AccessControlMock')
 
 contract('AccessControlMock', (accounts) => {
   it('', async () => {
-    const accessControlInstance = await AccessControlMock.deployed();
-    const adminRole = await accessControlInstance.DEFAULT_ADMIN_ROLE();
-    const account0 = '0x0000000000000000000000000000000000000000';
+    const accessControlInstance = await AccessControlMock.deployed()
+    const adminRole = await accessControlInstance.DEFAULT_ADMIN_ROLE()
+    const account0 = '0x0000000000000000000000000000000000000000'
     const account0IsAdmin = await accessControlInstance.hasRole(
       adminRole,
       account0
-    );
+    )
     const account1IsAdmin = await accessControlInstance.hasRole(
       adminRole,
       accounts[0]
-    );
-    assert.isNotTrue(account0IsAdmin, 'account0 is admin');
-    assert.isTrue(account1IsAdmin, 'account1 is admin');
-  });
-});
+    )
+    assert.isNotTrue(account0IsAdmin, 'account0 is admin')
+    assert.isTrue(account1IsAdmin, 'account1 is admin')
+  })
+})
